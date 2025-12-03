b42938205953129e3eda4d2138c603cf7ebb7820
misasoares <misasoaresms2@gmail.com>
2025-12-02 23:19:18 -0300

    feat(api): enable CORS for frontend integration

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,12 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: 'http://localhost:5173',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
